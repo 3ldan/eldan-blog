9350fc80e399636cf049a0ca5cc8b0c3e071a41f
Eldan Schoeman <EldanS@cjd.local>
2023-01-08 12:05:48 +0200

    Updated WTForms to v3.0.1 to include EmailField

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField
+from wtforms import StringField, EmailField, SubmitField, PasswordField
 from wtforms.validators import DataRequired, URL, Length, Email, Regexp
 from flask_ckeditor import CKEditorField
 
@@ -13,12 +13,12 @@ class CreatePostForm(FlaskForm):
 
 class RegisterForm(FlaskForm):
     name = StringField("Full Name", validators=[DataRequired()])
-    email = StringField("Email Address", validators=[DataRequired()])
+    email = EmailField("Email Address", validators=[DataRequired()])
     password = PasswordField("Password", validators=[Length(min=6), DataRequired()])
     register = SubmitField("Register")
 
 class LoginForm(FlaskForm):
-    email = StringField("Email Address", validators=[DataRequired()])
+    email = EmailField("Email Address", validators=[DataRequired()])
     password = PasswordField("Password", validators=[DataRequired()])
     login = SubmitField("Login")
 
